fd3b7601533adda5047eb78676d055dc081ef8ab
Alyshia Bentley <alyshiabentley@gmail.com>
2019-10-28 15:54:45 -0700

    completed module

--- src/app/track.ts
+++ src/app/track.ts
@@ -3,6 +3,6 @@ import {Track} from './track';
 export interface Track {
     trackNumber : number;
     trackName: string;
-    trackLength: string,;
+    trackLength: string;
     trackPRice: number;
 }
